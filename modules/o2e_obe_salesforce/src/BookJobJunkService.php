23b5c77c33fad93965911cb916d40f0721227747
Kapil Kumar <kapil@capitalnumbers.com>
2022-07-27 17:26:45 +0530

    WEB-4720: Create REST API service for BookJobJunk.

---
+++ modules/o2e_obe_salesforce/src/BookJobJunkService.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Drupal\o2e_obe_salesforce;
+
+use GuzzleHttp\Client;
+use Drupal\Core\Logger\LoggerChannelFactory;
+use Drupal\Core\State\State;
+use Drupal\Component\Serialization\Json;
+use GuzzleHttp\Exception\ClientException;
+use Drupal\Component\Utility\UrlHelper;
+use Drupal\Core\TempStore\PrivateTempStoreFactory;
+
+/**
+ * Promo Details Junk Service class is return the promo details.
+ */
+class BookJobJunkService {
+
+
+  /**
+   * PrivateTempStoreFactory definition.
+   *
+   * @var \Drupal\Core\TempStore\PrivateTempStoreFactory
+   */
+  protected $tempStoreFactory;
+
+  /**
+   * GuzzleHttp\Client definition.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
+
+  /**
+   * Logger Factory.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactory
+   */
+  protected $loggerFactory;
+
+  /**
+   * The object State.
+   *
+   * @var \Drupal\Core\State\State
+   */
+  protected $state;
+  /**
+   * The Auth Token Manager.
+   *
+   * @var \Drupal\o2e_obe_salesforce\AuthTokenManager
+   */
+  protected $authTokenManager;
+
+  /**
+   * Constructor method.
+   */
+  public function __construct(Client $http_client, LoggerChannelFactory $logger_factory, State $state, PrivateTempStoreFactory $temp_store_factory, AuthTokenManager $auth_token_manager) {
+    $this->httpClient = $http_client;
+    $this->loggerFactory = $logger_factory;
+    $this->state = $state;
+    $this->tempStoreFactory = $temp_store_factory;
+    $this->authTokenManager = $auth_token_manager;
+
+  }
+
+  /**
+   * Return the book job junk data.
+   */
+  public function bookJobJunk(array $options = []) {
+    $auth_token = $this->authTokenManager->getToken();
+    $endpoint_segment = $this->authTokenManager->getSfConfig('sf_book_job_junk.api_url_segment');
+    if (substr($endpoint_segment, 0, 1) !== '/') {
+      $endpoint_segment = '/' . $endpoint_segment;
+    }
+    $tempstore = $this->tempStoreFactory->get('o2e_obe_salesforce')->get('response');
+    $api_url = $this->state->get('sfUrl') . $endpoint_segment;
+
+    $headers = [
+      'Authorization' => $auth_token,
+      'content-type' => 'application/json',
+    ];
+
+    $options += [
+      'brand' => $this->authTokenManager->getSfConfig('sf_brand.brand'),
+      'franchise_id' => $tempstore['franchise_id'],
+    ];
+    try {
+      $response = $this->httpClient->request('POST', $api_url, [
+        'headers' => $headers,
+        'json' => $options,
+      ]);
+      $result = Json::decode($response->getBody(), TRUE);
+      $this->loggerFactory->get('Salesforce - Book Job Junk')->notice(UrlHelper::buildQuery($options) . ' ' . Json::encode($result));
+      return $result;
+    }
+    catch (ClientException $e) {
+      $this->loggerFactory->get('Salesforce - Book Job Junk Fail')->error($e->getMessage());
+    }
+  }
+
+}
