a502955e32b2c881d7a3ce8a7c9109d8a255bf31
Yash Bharadwaj <yashbharadwajsuper@gmail.com>
2022-08-01 18:29:32 +0530

    WEB-4411: Implemented datepicker logic with reactjs

---
+++ themes/o2e_obe_subtheme/obe-datepicker/src/components/AccordionItem.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+export default function AccordionItem({
+  timeOfTheDay,
+  accordionId,
+  itemsArray,
+}) {
+  const [isExpanded, setExpanded] = useState(false);
+  return (
+    <div className="accordion-item">
+      <h3
+        className="accordion-header"
+        id={`headingOne${accordionId}${timeOfTheDay}`}
+      >
+        {itemsArray[timeOfTheDay].length ? (
+          <button
+            onClick={() => setExpanded(false)}
+            className={`accordion-button ${timeOfTheDay ? "collapsed" : ""}`}
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target={`#collapseOne${accordionId}${timeOfTheDay}`}
+            aria-expanded={timeOfTheDay ? "false" : "true"}
+            aria-controls={`collapseOne${accordionId}${timeOfTheDay}`}
+          >
+            <p>
+              <strong>{timeOfTheDay}</strong>
+              <span>
+                {itemsArray[timeOfTheDay].length} timeslots available.
+              </span>
+            </p>
+          </button>
+        ) : (
+          <p className="accordion-button collapsed">
+            <strong>{timeOfTheDay}</strong>
+            <span>{itemsArray[timeOfTheDay].length} timeslots available.</span>
+          </p>
+        )}
+      </h3>
+      {itemsArray[timeOfTheDay].length ? (
+        <div
+          id={`collapseOne${accordionId}${timeOfTheDay}`}
+          className={`accordion-collapse collapse ${
+            timeOfTheDay ? "" : "show"
+          }`}
+          aria-labelledby={`headingOne${accordionId}${timeOfTheDay}`}
+          data-bs-parent={`#accordion${accordionId}`}
+        >
+          <div className="accordion-body">
+            {isExpanded
+              ? itemsArray[timeOfTheDay]
+              : itemsArray[timeOfTheDay].slice(0, 4)}
+            <span
+              className="btn-expand"
+              onClick={() => setExpanded(!isExpanded)}
+            >
+              {isExpanded ? "Less" : "More"}
+            </span>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
