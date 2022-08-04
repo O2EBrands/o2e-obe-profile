d9aad3b720f478c52d1d1801ad7678e3ea6d7d2c
Yash Bharadwaj <yashbharadwajsuper@gmail.com>
2022-08-04 19:23:35 +0530

    WEB-4411: updated datepicker fields logic

--- themes/o2e_obe_subtheme/obe-datepicker/src/components/Loader.js
+++ themes/o2e_obe_subtheme/obe-datepicker/src/components/Loader.js
@@ -1,10 +1,13 @@
 function Loader() {
   return (
     <div className="obe-loader">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <span className="loader-title"> Checking available time slots... </span>
+      <div className="obe-loader-circles">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 }
