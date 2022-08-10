06899ce05809d38e633fa31986e8be4326c22ce5
Yash Bharadwaj <yashbharadwajsuper@gmail.com>
2022-08-10 13:07:26 +0530

    Enabled ajax and Integrated behaviours

--- themes/o2e_obe_subtheme/obe-datepicker/src/components/SlotLoader.js
+++ themes/o2e_obe_subtheme/obe-datepicker/src/components/SlotLoader.js
@@ -8,12 +8,12 @@ let renderArray = [];
 for (let i = 0; i < 3; i++) {
   accordionItems.push(
     <div className="accordion-item">
-      <h3 className="accordion-header" id="headingOne1morning">
+      <div className="accordion-header" id="headingOne1morning">
         <p
           className="accordion-button collapsed"
           style={{ height: "80px" }}
         ></p>
-      </h3>
+      </div>
     </div>
   );
 }
@@ -22,7 +22,7 @@ for (let i = 0; i < 3; i++) {
 for (let i = 0; i < 3; i++) {
   renderArray.push(
     <div className="col-lg-4 accordion">
-      <h3 className="slot-day-title" style={{ height: "54px" }}></h3>
+      <p className="slot-day-title" style={{ height: "54px" }}></p>
       {accordionItems}
     </div>
   );
