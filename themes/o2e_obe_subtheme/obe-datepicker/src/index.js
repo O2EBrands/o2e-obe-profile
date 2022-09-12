06f16ff430e15bb247125c126fbb928e3e49de40
Yash Bharadwaj <yashbharadwajsuper@gmail.com>
2022-09-12 18:14:56 +0530

    Datepicker optimization: Once implementation

--- themes/o2e_obe_subtheme/obe-datepicker/src/index.js
+++ themes/o2e_obe_subtheme/obe-datepicker/src/index.js
@@ -10,18 +10,20 @@ import reportWebVitals from "./reportWebVitals";
         'div[data-drupal-selector="edit-react-datepicker-component"]'
       );
 
-      if (reactMountPoint) {
-        const root = ReactDOM.createRoot(reactMountPoint);
-        root.render(
-          <React.StrictMode>
-            <App />
-          </React.StrictMode>
-        );
-        // If you want to start measuring performance in your app, pass a function
-        // to log results (for example: reportWebVitals(console.log))
-        // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-        reportWebVitals();
-      }
+      once("reactDatepicker", "html", context).forEach(function (element) {
+        if (reactMountPoint) {
+          const root = ReactDOM.createRoot(reactMountPoint);
+          root.render(
+            <React.StrictMode>
+              <App />
+            </React.StrictMode>
+          );
+          // If you want to start measuring performance in your app, pass a function
+          // to log results (for example: reportWebVitals(console.log))
+          // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+          reportWebVitals();
+        }
+      });
     },
   };
-})(jQuery, Drupal);
+})(jQuery, Drupal, once);
