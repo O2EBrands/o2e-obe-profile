fc4d9a8c16dcce451ecda4e546304a6b67931d8d
Kapil Kumar <kapil.kumar.innoraft.com>
2022-10-10 15:07:31 +0530

    WEB-4424: fixed the hold time workflow.

--- modules/o2e_obe_form/src/Plugin/WebformHandler/AvailableTimesVerification.php
+++ modules/o2e_obe_form/src/Plugin/WebformHandler/AvailableTimesVerification.php
@@ -54,6 +54,13 @@ class AvailableTimesVerification extends ObeWebformHandlerBase {
   protected $messenger;
 
   /**
+   * HoldTimeService Manager.
+   *
+   * @var \Drupal\o2e_obe_salesforce\HoldTimeService
+   */
+  protected $holdTimeService;
+
+  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
@@ -62,6 +69,7 @@ class AvailableTimesVerification extends ObeWebformHandlerBase {
     $instance->tempStoreFactory = $container->get('tempstore.private');
     $instance->config = $container->get('config.factory');
     $instance->messenger = $container->get('messenger');
+    $instance->holdTimeService = $container->get('o2e_obe_salesforce.hold_time');
     return $instance;
   }
 
@@ -80,27 +88,58 @@ class AvailableTimesVerification extends ObeWebformHandlerBase {
   public function validateForm(array &$form, FormStateInterface $formState, WebformSubmissionInterface $webform_submission) {
     $current_page = $formState->get('current_page');
     $selected_step = $this->configuration['steps'];
+    $sfresponse = $this->tempStoreFactory->get('o2e_obe_salesforce');
     if ($current_page === $selected_step) {
-      // Check Expiry.
-      $checkExpiry = check_local_time_expiry();
-      if ($checkExpiry) {
-        // Set the slotHoldTime tempstore to TRUE.
-        $this->tempStoreFactory->get('o2e_obe_salesforce')->set('slotHoldTime', TRUE);
-        return TRUE;
+      $hotd_time_success = $sfresponse->get('slotHoldTimesuccess');
+      $start_form_date = $formState->getValue('start_date_time');
+      $end_form_date = $formState->getValue('finish_date_time');
+      $holdSlotTime = $sfresponse->get('holdSlotTime');
+      if ($hotd_time_success == TRUE && (isset($holdSlotTime['start_date_time']) && isset($holdSlotTime['start_date_time'])) && ($holdSlotTime['start_date_time'] == $start_form_date && $holdSlotTime['finish_date_time'] == $end_form_date)) {
+        // Check Expiry.
+        $checkExpiry = check_local_time_expiry();
+        if ($checkExpiry) {
+          $sfresponse->set('slotHoldTime', TRUE);
+          return TRUE;
+        }
+        else {
+          $this->display_expiry_message($form, $formState, $sfresponse);
+        }
       }
       else {
-        // Redirect to selected redirected step.
-        $form['elements'][$selected_step]['holdtime_data']['#access'] = FALSE;
-        $this->tempStoreFactory->get('o2e_obe_salesforce')->set('slotHoldTime', FALSE);
-        $pages = $formState->get('pages');
-        $redirect_to_step = $this->configuration['redirect_to_step'];
-        goto_step($redirect_to_step, $pages, $formState);
-        // Show slot expiry message.
-        $slot_expiry_message = $this->config->get('o2e_obe_common.settings')->get('o2e_obe_common')['slot_holdtime_expiry_message'];
-        $this->messenger()->addError($slot_expiry_message);
-        return FALSE;
+        $options = [
+          'start_date_time' => $start_form_date,
+          'finish_date_time' => $end_form_date,
+        ];
+        $holdTimeResponse = $this->holdTimeService->holdtime($options);
+        if ($holdTimeResponse == TRUE) {
+          $sfresponse->set('holdSlotTime', $options);
+          $sfresponse->set('slotHoldTimesuccess', TRUE);
+          $sfresponse->set('slotHoldTime', TRUE);
+
+        }
+        else {
+          $sfresponse->delete('holdSlotTime');
+          $this->display_expiry_message($form, $formState, $sfresponse);
+        }
       }
+
     }
   }
 
+  /**
+  * Redirect to selected redirected step.
+  */
+  private function display_expiry_message(array &$form, FormStateInterface $formState, $sfresponse) {
+    $form['elements'][$selected_step]['holdtime_data']['#access'] = FALSE;
+    $sfresponse->set('slotHoldTime', FALSE);
+    $sfresponse->set('slotHoldTimesuccess', FALSE);
+    $pages = $formState->get('pages');
+    $redirect_to_step = $this->configuration['redirect_to_step'];
+    goto_step($redirect_to_step, $pages, $formState);
+    // Show slot expiry message.
+    $slot_expiry_message = $this->config->get('o2e_obe_common.settings')->get('o2e_obe_common')['slot_holdtime_expiry_message'];
+    $this->messenger()->addError($slot_expiry_message);
+    return FALSE;
+  }
 }
+
